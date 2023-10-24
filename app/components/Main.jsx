61902f2d747ce5796643bbc60812168e03aa1848
unknown <tolunay17>
2023-10-24 17:37:39 +0200

    deploy

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation'
 import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
+import Image from 'next/image'
 const Main = () => {
   return (
     <div id='main'>
-        <img className='w-full h-screen object-cover object-left scale-x-[1]' src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?auto=format&fit=crop&q=80&w=1955&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+        <Image alt='random' className='w-full h-screen object-cover object-left scale-x-[1]' src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?auto=format&fit=crop&q=80&w=1955&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
         <div className='w-full h-screen absolute top-0 left-0 bg-white/40'>
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg-items-start items-center'>
-            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Tolunay Kutlu</h1>
-            <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a 
+            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I&apos;m Tolunay Kutlu</h1>
+            
+            <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I&apos;m a 
             <TypeAnimation
                 sequence={[
                     // Same substring at the start will only be typed out once, initially
--- app/components/ProjectItem.jsx
+++ app/components/ProjectItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Image from 'next/image'
 const ProjectItem = ({image, title}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#000]'>
-      <img src={image} alt="/" className='rounded-xl group-hover:opacity-10' />
+      <Image src={image} alt="/" className='rounded-xl group-hover:opacity-10' />
       <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h2 className='text-2xl font-bold text-white tracking-wider text-center'>
           {title}
