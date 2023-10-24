6fe9c3059f8c5947a9f5f7dbbaa2747e6f8e53a6
unknown <tolunay17>
2023-10-24 17:49:01 +0200

    destiryed sidenav

--- app/components/index.js
+++ app/components/index.js
@@ -2,7 +2,7 @@ import Contact from './Contact';
 import Main from './Main';
 import ProjectItem from './ProjectItem';
 import Projects from './Projects';
-import SideNav from './SideNav';
+
 import Work from './Work'
 import WorkItem from './WorkItem';
 
@@ -11,7 +11,7 @@ export {
     Main,
     ProjectItem,
     Projects,
-SideNav,
+
     Work,
     WorkItem
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import Image from 'next/image'
-import {SideNav,Main,Work,Projects,Contact} from '@/app/components/'
+import {Main,Work,Projects,Contact} from '@/app/components/'
 export default function Home() {
   return (
 <>
-<SideNav />
+{/* <SideNav /> */}
 <Main />
 <Work />
 <Projects />
